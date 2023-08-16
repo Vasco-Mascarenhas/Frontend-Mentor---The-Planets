c5273d64fc13abb9c31c6e4357f3f4f5dd081945
vasco.mascarenhas <vasco.mascarenhas@hotmail.com>
2023-08-16 15:55:39 +0100

    + logic to navbar + body bg + miscellaneous

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 import "./App.css";
 import NavBar from "./components/navbar/NavBar";
-
+import data from "./../data.json";
+console.log(data);
 function App() {
+  const handleNavClick = (link) => {
+    console.log(link);
+  };
+
   return (
     <>
-      <NavBar />
+      <NavBar
+        onNavClick={handleNavClick}
+        items={data.map((item) => item.name)}
+      />
     </>
   );
 }
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import "./navbar.css";
-const NavBar = ({ items }) => {
-  return <div></div>;
+const NavBar = ({ items, onNavClick }) => {
+  return (
+    <header>
+      <nav className="container">
+        <h2>THE PLANETS</h2>
+        <ul>
+          {items?.map((item) => (
+            <li onClick={() => onNavClick(item)} key={item}>
+              {item}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default NavBar;
