3ebae191e2b18c0ece8afe31f571847cb452fe5d
vasco.mascarenhas <vasco.mascarenhas@hotmail.com>
2023-08-16 15:59:32 +0100

    + planet component + planet selection logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import "./App.css";
 import NavBar from "./components/navbar/NavBar";
 import data from "./../data.json";
+import Planet from "./components/planet/Planet";
 console.log(data);
 function App() {
+  const [planet, setPlanet] = useState("");
   const handleNavClick = (link) => {
-    console.log(link);
+    setPlanet(link);
   };
 
   return (
@@ -14,6 +16,7 @@ function App() {
         onNavClick={handleNavClick}
         items={data.map((item) => item.name)}
       />
+      <Planet planet={planet} />
     </>
   );
 }
---
+++ src/components/planet/Planet.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Planet = ({ planet }) => {
+  return <div>{planet}</div>;
+};
+
+export default Planet;
